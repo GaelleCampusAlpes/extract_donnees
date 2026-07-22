2efddde8fb52b9b18459bda29395c982a976c4f2
GaelleCampusAlpes <gaelle.giroux@le-campus-numerique.fr>
2026-07-22 09:29:19 +0000

    test push

--- bronze/Notebooks_extract_donnee.py
+++ bronze/Notebooks_extract_donnee.py
@@ -5,18 +5,3 @@
 # ///
 # MAGIC %md
 # MAGIC Notebook extract de données databricks 
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC
-# MAGIC **import fichier csv** 
-# MAGIC
-# MAGIC DEPUIS UN VOLUME
-# MAGIC
-# MAGIC
-
-# COMMAND ----------
-
-path = "/Volumes/databricks_gaelle/default/donnees_api/ent_comm_detail.csv"
-### " test connexion"
